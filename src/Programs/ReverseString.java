bcc40a30ea8a5112cdfbd68a8e3d84de527ab61e
laxminarayan.sharma <laxminarayan.sharma@hapana.com>
2025-03-19 22:52:20 +0530

    variable name change in the Rev string

--- src/Programs/ReverseString.java
+++ src/Programs/ReverseString.java
@@ -5,13 +5,13 @@ public class ReverseString {
 	public static void main(String[] args) {
 
 		String name = "Laxminarayan";
-		String rev = "";
+		String rev1 = "";
 
 		for (int i = name.length()-1; i >= 0; i--) {
-			rev = rev + name.charAt(i);
+			rev1 = rev1 + name.charAt(i);
 			
 		}
-		System.out.println(rev);
+		System.out.println(rev1);
 	}
 
 }
