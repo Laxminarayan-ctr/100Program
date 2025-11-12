bb221cdb0aa50cd2b190b9ca500746f57359110f
Laxminarayan <laxminarayan@universityliving.com>
2025-11-12 18:35:20 +0530

    fix variable mistake

--- src/Programs/MissingVAarry.java
+++ src/Programs/MissingVAarry.java
@@ -1,34 +1,25 @@
 package Programs;
 
-public class MissingVAarry{
-	
-	public static void main(String[] args)
-	{
-		int num[] = {1,2,3,4,6};
-		
-		int sum= 0;
+public class MissingVAarry {
+
+	public static void main(String[] args) {
+		int num[] = {1,2,3,4,5,7};
+
+		int sum = 0;
 		int sum1 = 0;
 		
-		
-		for(int i=0; i<num.length; i++)
+		for(int i= 0; i<num.length;i++)
 		{
-			sum = sum+num[i];
+			sum= sum+num[i];
 		}
 		
-		for(int k=0; k<=num.length;k++)
+		for(int k=0; k<=num.length+1;k++)
 		{
-			sum1 = sum+k;
+			sum1 = sum1+k;
 		}
 		
-		int miss = sum1-sum;
-		
-		System.out.println(miss);
-		
-		
-		
-		
+		int diff = sum1-sum;
+		System.out.println(diff);
 	}
-	
-	
-	
+
 }
